babd9827066b83d56b59cf53bdccf05469893820
devmyschoollunch <devmyschoollunch@gmail.com>
2018-09-02 21:32:05 -0400

    fd

--- src/components/parts/register.jsx
+++ src/components/parts/register.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import $ from "jquery";
 import CommonBuildParams from "../../scripts/JSmain";
-import PartsCommonUSStates from "../parts/common/usStates";
 
 class PartsRegister extends Component {
   constructor(props) {
@@ -20,14 +19,7 @@ class PartsRegister extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  _handleSelectedIndex = counter => {
-    const counters = [...this.state.counters]; // ... is a spread operator which  clones the object
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    console.log(counters[index].value++);
-    this.setState({ counters });
-  };
+
   handleSubmit(e) {
     const s = this.state;
     e.preventDefault();
@@ -50,6 +42,20 @@ class PartsRegister extends Component {
     $.post("http://localhost:60769/Home/About?data=" + JSON.stringify(data));
   }
 
+  handleChange(event) {
+    let e = event.target;
+    this.setState({
+      Firstname: e.value,
+      Firstname: e.value,
+      Lastname: e.value,
+      Username: e.value,
+      Email: e.value,
+      Address: e.value,
+      Address2: e.value,
+      USState: e.value,
+      Zipcode: e.value
+    });
+  }
   render() {
     return (
       <div>
@@ -138,65 +144,65 @@ class PartsRegister extends Component {
                   value={this.state.value}
                   onChange={this.handleChange}
                 /> */}
-  <select
-          value={this.state.value}
-          onChange={this.handleChange}
-          className="custom-select mr-sm-2"
-        >
-          <option value="">Pick a State...</option>
-          <option value="AL">Alabama</option>
-          <option value="AK">Alaska</option>
-          <option value="AZ">Arizona</option>
-          <option value="AR">Arkansas</option>
-          <option value="CA">California</option>
-          <option value="CO">Colorado</option>
-          <option value="CT">Connecticut</option>
-          <option value="DE">Delaware</option>
-          <option value="DC">District Of Columbia</option>
-          <option value="FL">Florida</option>
-          <option value="GA">Georgia</option>
-          <option value="HI">Hawaii</option>
-          <option value="ID">Idaho</option>
-          <option value="IL">Illinois</option>
-          <option value="IN">Indiana</option>
-          <option value="IA">Iowa</option>
-          <option value="KS">Kansas</option>
-          <option value="KY">Kentucky</option>
-          <option value="LA">Louisiana</option>
-          <option value="ME">Maine</option>
-          <option value="MD">Maryland</option>
-          <option value="MA">Massachusetts</option>
-          <option value="MI">Michigan</option>
-          <option value="MN">Minnesota</option>
-          <option value="MS">Mississippi</option>
-          <option value="MO">Missouri</option>
-          <option value="MT">Montana</option>
-          <option value="NE">Nebraska</option>
-          <option value="NV">Nevada</option>
-          <option value="NH">New Hampshire</option>
-          <option value="NJ">New Jersey</option>
-          <option value="NM">New Mexico</option>
-          <option value="NY">New York</option>
-          <option value="NC">North Carolina</option>
-          <option value="ND">North Dakota</option>
-          <option value="OH">Ohio</option>
-          <option value="OK">Oklahoma</option>
-          <option value="OR">Oregon</option>
-          <option value="PA">Pennsylvania</option>
-          <option value="RI">Rhode Island</option>
-          <option value="SC">South Carolina</option>
-          <option value="SD">South Dakota</option>
-          <option value="TN">Tennessee</option>
-          <option value="TX">Texas</option>
-          <option value="UT">Utah</option>
-          <option value="VT">Vermont</option>
-          <option value="VA">Virginia</option>
-          <option value="WA">Washington</option>
-          <option value="WV">West Virginia</option>
-          <option value="WI">Wisconsin</option>
-          <option value="WY">Wyoming</option>
-        </select>
-    
+                <select
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                  className="custom-select mr-sm-2"
+                >
+                  <option value="">Pick a State...</option>
+                  <option value="AL">Alabama</option>
+                  <option value="AK">Alaska</option>
+                  <option value="AZ">Arizona</option>
+                  <option value="AR">Arkansas</option>
+                  <option value="CA">California</option>
+                  <option value="CO">Colorado</option>
+                  <option value="CT">Connecticut</option>
+                  <option value="DE">Delaware</option>
+                  <option value="DC">District Of Columbia</option>
+                  <option value="FL">Florida</option>
+                  <option value="GA">Georgia</option>
+                  <option value="HI">Hawaii</option>
+                  <option value="ID">Idaho</option>
+                  <option value="IL">Illinois</option>
+                  <option value="IN">Indiana</option>
+                  <option value="IA">Iowa</option>
+                  <option value="KS">Kansas</option>
+                  <option value="KY">Kentucky</option>
+                  <option value="LA">Louisiana</option>
+                  <option value="ME">Maine</option>
+                  <option value="MD">Maryland</option>
+                  <option value="MA">Massachusetts</option>
+                  <option value="MI">Michigan</option>
+                  <option value="MN">Minnesota</option>
+                  <option value="MS">Mississippi</option>
+                  <option value="MO">Missouri</option>
+                  <option value="MT">Montana</option>
+                  <option value="NE">Nebraska</option>
+                  <option value="NV">Nevada</option>
+                  <option value="NH">New Hampshire</option>
+                  <option value="NJ">New Jersey</option>
+                  <option value="NM">New Mexico</option>
+                  <option value="NY">New York</option>
+                  <option value="NC">North Carolina</option>
+                  <option value="ND">North Dakota</option>
+                  <option value="OH">Ohio</option>
+                  <option value="OK">Oklahoma</option>
+                  <option value="OR">Oregon</option>
+                  <option value="PA">Pennsylvania</option>
+                  <option value="RI">Rhode Island</option>
+                  <option value="SC">South Carolina</option>
+                  <option value="SD">South Dakota</option>
+                  <option value="TN">Tennessee</option>
+                  <option value="TX">Texas</option>
+                  <option value="UT">Utah</option>
+                  <option value="VT">Vermont</option>
+                  <option value="VA">Virginia</option>
+                  <option value="WA">Washington</option>
+                  <option value="WV">West Virginia</option>
+                  <option value="WI">Wisconsin</option>
+                  <option value="WY">Wyoming</option>
+                </select>
+
 
               </div>
               <div className="col-md-3 mb-3">
@@ -219,20 +225,7 @@ class PartsRegister extends Component {
     );
   }
 
-  handleChange(event) {
-    let e = event.target;
-    this.setState({
-      Firstname: e.value,
-      Firstname: e.value,
-      Lastname: e.value,
-      Username: e.value,
-      Email: e.value,
-      Address: e.value,
-      Address2: e.value,
-      USState: e.value,
-      Zipcode: e.value
-    });
-  }
+
 }
 
 export default PartsRegister;
